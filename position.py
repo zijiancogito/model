fc38a130961e82a64928473d38d5bc86158c29bb
cogitozijian <345981440@qq.com>
2020-08-21 15:16:32 +0800

    f

--- position.py
+++ position.py
@@ -7,6 +7,7 @@ from torch.autograd import Variable
 
 class PositionalEncoding(nn.Module):
   def __init__(self, d_model, dropout, max_len=5000):
+    super(PositionalEncoding, self).__init__()
     self.dropout = nn.Dropout(p=dropout)
     pe = torch.zeros(max_len, d_model)
     position = torch.arange(0, max_len).unsqueeze(1)
