fb8ea11854f19bc2abbecf0a60fcb40600b2fcc9
cogitozijian <345981440@qq.com>
2020-08-21 16:26:26 +0800

    f

--- embedding.py
+++ embedding.py
@@ -35,7 +35,7 @@ def average_batch(p_batch, p_x, split_index, pad_idx, p_pad_emb):
   t_batch_list = []
   for emb, index in zip(batch, x):
     t_batch_list.append(average_emb(emb, index, split_index, pad_idx))
-  max_len = max([len(emb) for emb in t_batch_list])
+  max_len = max([len(emb) for emb in t_batch_list]) + 1
   # max_len = p_batch.shape[-2]
   batch_list = []
   for emb in t_batch_list:
