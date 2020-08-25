440395745ddad170cfcfb022981c75cd44550203
cogitozijian <345981440@qq.com>
2020-08-25 12:40:43 +0800

    fix bugs

--- model_utils.py
+++ model_utils.py
@@ -48,6 +48,18 @@ def run_epoch(data_iter, model, loss_compute):
   tokens = 0
   for i, batch in enumerate(data_iter):
     out = model.forward(batch.src, batch.trg, batch.src_mask, batch.trg_mask)
+
+    # Compute BLEU
+    prob = model.generator(out[:, -1])
+    _, next_word = torch.max(prob, dim=1)
+    next_word = next_word.data[0]
+    ys = torch.cat([ys,
+                    torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+    print(ys)
+    import pdb
+    pdb.set_trace()
+
+    # Compute LOSS
     loss = loss_compute(out, batch.trg_y, batch.ntokens)
     total_loss += loss
     total_tokens += batch.ntokens
