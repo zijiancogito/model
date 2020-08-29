e0dc41d235530761de00252ba113137c4e2b6f63
cogitozijian <345981440@qq.com>
2020-08-30 01:11:09 +0800

    fix bugs

--- param.py
+++ param.py
@@ -4,12 +4,12 @@ LAYER_NUM = 1
 D_MODEL = 512
 SRC_TOKEN_LEN = 2
 TRG_TOEKN_LEN = 1
-SRC_TOKEN = 4
-BATCH_SIZE = 5
+SRC_TOKEN = 8
+BATCH_SIZE = 20
 H = 8
 
 LABEL_SMOOTH = 0.1
 
-TRAIN_FILE = '../data/8_29/train.csv'
-VAL_FILE = '../data/8_29/vali.csv'
-TEST_FILE = '../data/8_29/test.csv'
+TRAIN_FILE = '../data/8_29/con_train.csv'
+VAL_FILE = '../data/8_29/con_vali.csv'
+TEST_FILE = '../data/8_29/con_test.csv'
